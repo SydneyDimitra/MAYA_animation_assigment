d7347c911464403e40cedc1e5511f1c362141239
SydneyDimitra <i7658072@bournemouth.ac.uk>
2017-07-23 14:30:39 +0300

    final_version

--- staircase.py
+++ staircase.py
@@ -1,8 +1,40 @@
 # importing Python Maya commands
 import maya.cmds as cmds
 
+# create the first step
+cmds.polyCube(n='step',sx=1, sy=1, sz=1, h=0.2)
+# duplicate step - total 26 steps 
+list=[]
+for i in range(0, 26):
+    clone = cmds.duplicate('step')
+    list.append(clone[0])
+# move steps to create the first section of staircase
+x=0
+y=0
+z=0   
+for i in range(0,9):   
+    cmds.move(x, y, z, list[i])
+    z+=1
+    y+=0.2
+# second section of staircase
+x-=1
+z-=1
+for i in range(9, 18):
+    cmds.move(x, y, z, list[i])
+    x-=1
+    y+=0.2
+# third section of staircase
+z-=1 
+x+=1   
+for i in range(18, 21):
+    cmds.move(x, y, z, list[i])
+    z-=1
+    y+=0.2
+# fourth section of staircase
+x+=1  
+z+=1  
+for i in range(21, 26):
+    cmds.move(x, y, z, list[i])
+    x+=1
+    y+=0.2
 
-
-# clean the history
-cmds.delete(dome, ch=1)
-cmds.delete(left_dome, ch=1)
